8a20938be0dfa82b4385c45622aa8994b709c1cc
Nick Johnson <arachnid@notdot.net>
2019-08-15 12:30:22 +1200

    Use local repository for build
    This makes it possible to use docker locally to test builds, and to build on branches other than the current master on github.

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 set -e
 
-SRCDIR=`pwd`
-BUILDDIR=`mktemp -d`
+SRCDIR=/
+BUILDDIR=/build
 
 mkdir -p ${BUILDDIR} 2>/dev/null
 cd ${BUILDDIR}
+echo "Cloning coredns repo..."
 git clone https://github.com/coredns/coredns.git
+
 cd coredns
 git checkout v1.6.1
+
+echo "Patching plugin config..."
 ed plugin.cfg <<EOED
 21a
 ens:github.com/wealdtech/coredns-ens
@@ -16,11 +20,16 @@ ens:github.com/wealdtech/coredns-ens
 w
 q
 EOED
+
 # The Kubernetes dependencies result in an invalid package so replace them
+echo "Patching go modules..."
 cp go.mod go.mod.orig
 egrep -v k8s.io/ go.mod.orig >go.mod
 ed go.mod <<EOED
-51a
+a
+replace github.com/wealdtech/coredns-ens => ../../coredns-ens
+.
+52a
 k8s.io/api kubernetes-1.14.1
 k8s.io/apimachinery kubernetes-1.14.1
 k8s.io/client-go kubernetes-1.14.1
@@ -29,8 +38,10 @@ github.com/wealdtech/coredns-ens v1.1.0
 w
 q
 EOED
+
+echo "Building..."
 make SHELL='sh -x' CGO_ENABLED=1 coredns
+
 cp coredns ${SRCDIR}
 cd ${SRCDIR}
-chmod -R 755 ${BUILDDIR}/coredns/.git/objects/pack
 rm -r ${BUILDDIR}
